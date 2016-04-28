c58b9baa774088866643e7c5fd8936f18ac5ac8c
chad <chadhokama@gmail.com>
2016-04-28 14:43:51 -0700

    changing setup to reflect full forking of the project

--- setup.py
+++ setup.py
@@ -1,28 +1,11 @@
 from setuptools import setup
 
-version = '0.3'
+version = '0.1'
 
-setup(name='bitly_api',
+setup(name='hpx-bitly',
       version=version,
-      description="An API for bit.ly",
-      long_description=open("./README.md", "r").read(),
-      classifiers=[
-          "Development Status :: 5 - Production/Stable",
-          "Environment :: Console",
-          "Intended Audience :: End Users/Desktop",
-          "Natural Language :: English",
-          "Operating System :: OS Independent",
-          "Programming Language :: Python",
-          "Topic :: Internet :: WWW/HTTP :: Dynamic Content :: CGI Tools/Libraries",
-          "Topic :: Utilities",
-          "License :: OSI Approved :: Apache Software License",
-          ],
-      keywords='bitly bit.ly',
-      author='Jehiah Czebotar',
-      author_email='jehiah@gmail.com',
-      url='https://github.com/bitly/bitly-api-python',
-      license='Apache Software License',
-      packages=['bitly_api'],
+      description="An API for bit.ly. Forked from bitly-api-python which is not updating the project",
+      packages=['hpx-bitly'],
       include_package_data=True,
       zip_safe=True,
       )
